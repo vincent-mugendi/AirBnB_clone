7c09e8db5afc55992ca8b736a49dc25f6dc9cfe1
vincent-mugendi <vincentmwathi5@gmail.com>
2023-10-11 15:17:15 +0000

    Edited base_model.py according to no. 4 requirements

--- models/base_model.py
+++ models/base_model.py
@@ -13,35 +13,44 @@ class BaseModel:
 	updated_at (datetime): Timestamp for instance update
 	"""
 
-	def __init__(self):
+	def __init__(self, *args, **kwargs):
 		"""
 		Initializes new instance of the BaseModel class
-		"""
-		self.id = str(uuid.uuid4()) # Generete a new ID and convert it to a string
-		self.created_at = datetime.now() # Set created_at attribute with the current date and time
-		self.updated_at = datetime.now() # Set upadated_at attributewith the current date
 
-	def __str__(self):
+		Args:
+			*args: Variables length argument list (unused)
+			**kwargs: Arbitrary keyword arguments to create the instance from a dictionary representation
 		"""
-		Returns a string representation of the object
+
+		# Set default values for the `id`, `created_at`, and `updated_at` attributes.
+		kwargs.setdefault("id", str(uuid.uuid4()))
+		kwargs.setdefault("created_at", datetime.now())
+		kwargs.setdefault("updated_at", datetime.now())
+
+		# Set the object attributes.
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+	def __str__(self) -> str:
+		"""
+		Converts the object to a string
 		"""
 		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-	def save(self):
+	def save(self) -> None:
 		"""
-		Updates the updated_at attribute with the current datetime
+		Sets the updated_at attribute to the current datetime
 		"""
 		self.updated_at = datetime.now() # updates the update_at attribute with current datetime
 
 	def to_dict(self):
 		"""
-		Converts the object attributes to a dictionary
-
-		Returns:
-		dict: Dictionary containing object attributes.
+		Serializes the object to a dictionary.
 		"""
-		obj_dict = self.__dict__.copy()
-		obj_dict['__class__'] = self.__class__.__name__
-		obj_dict['created_at'] = self.created_at.isoformat()
-		obj_dict['updated_at'] = self.updated_at.isoformat()
+		obj_dict = {
+			key: value
+			for key, value in self.__dict__.items()
+			if not key.startswith("__")
+		}
+		obj_dict["__class__"] = self.__class__.__name__
 		return obj_dict
