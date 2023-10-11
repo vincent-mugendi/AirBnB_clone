ea812174bf08b7641901e599a7bc0731fc14b1fb
vincent-mugendi <vincentmwathi5@gmail.com>
2023-10-11 11:27:56 +0000

    Edited test_base_model.py to use unnittest module

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,16 +3,51 @@
 import sys
 sys.path.append('../')
 
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+	"""
+	Unit tests for the BaseModel class
+	"""
+
+	def setUp(self):
+		"""
+		Set up a BaseModel instance for testing.
+		"""
+		self.my_model = BaseModel()
+		self.my_model.name = "My First Model"
+		self.my_model.my_number = 89
+
+	def test_object_creation(self):
+		"""
+		Test the creation of BaseModel instance and its attributes
+		"""
+		self.assertIsInstance(self.my_model, BaseModel)
+		self.assertEqual(self.my_model.name, "My First Model")
+		self.assertEqual(self.my_model.my_number, 89)
+
+	def test_save_method(self):
+		"""
+		Test the save method to ensure updated_at attribute changes.
+		"""
+		initial_updated_at = self.my_model.updated_at
+		self.my_model.save()
+		self.assertNotEqual(self.my_model.updated_at, initial_updated_at)
+
+	def test_to_dict_method(self):
+		"""
+		Test the to_dict method for proper dictionary generation.
+		"""
+		obj_dict = self.my_model.to_dict()
+		self.assertIsInstance(obj_dict, dict)
+		self.assertIn('__class__', obj_dict)
+		self.assertEqual(obj_dict['__class__'], 'BaseModel')
+		self.assertIn('created_at', obj_dict)
+		self.assertIn('updated_at', obj_dict)
+		self.assertIn('id', obj_dict)
+		self.assertIn('name', obj_dict)
+		self.assertIn('my_number', obj_dict)
+
+if __name__ == "__main__":
+	unittest.main()
