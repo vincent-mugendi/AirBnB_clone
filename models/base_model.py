4f7d25de7dd1122cdd4b4fa6995612a30ae7a35d
vincent-mugendi <vincentmwathi5@gmail.com>
2023-10-11 17:04:58 +0000

    Added tests for no. 4

--- models/base_model.py
+++ models/base_model.py
@@ -27,9 +27,15 @@ class BaseModel:
 		kwargs.setdefault("created_at", datetime.now())
 		kwargs.setdefault("updated_at", datetime.now())
 
+		# Extract and remove the __class__ attribute
+		class_name = kwargs.pop("__class__", self.__class__.__name__)
+
 		# Set the object attributes.
 		for key, value in kwargs.items():
 			setattr(self, key, value)
+
+		# Dynamically set the class of the instance
+		self.__class__ = type(class_name, (BaseModel,), self.__dict__)
 
 	def __str__(self) -> str:
 		"""
@@ -47,10 +53,28 @@ class BaseModel:
 		"""
 		Serializes the object to a dictionary.
 		"""
-		obj_dict = {
-			key: value
-			for key, value in self.__dict__.items()
-			if not key.startswith("__")
-		}
+		obj_dict = self.__dict__.copy()
+
+		#convert the 'created_at and updated_at attributes to ISO format
+		obj_dict["created_at"] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+		obj_dict["updated_at"] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+		# Add the __class__ attributes to the dictionary
 		obj_dict["__class__"] = self.__class__.__name__
+
 		return obj_dict
+
+	@classmethod
+	def from_dict(cls, obj_dict):
+		"""
+		Creates an instance of the class from a dictionary representation.
+
+		Args:
+			obj_dict (dict): Dictionary containing object attributes.
+		Returns:
+			cls: Instance of the class.
+		"""
+		class_name = obj_dict.pop("__class__", cls.__name__)
+		obj_dict["created_at"] = datetime.strptime(obj_dict["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+		obj_dict["updated_at"] = datetime.strptime(obj_dict["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+		return cls(__class__=class_name, **obj_dict)
