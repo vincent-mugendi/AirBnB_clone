94db4ce6f2ee58a2035720c70e71ac47547af5d6
vincent-mugendi <vincentmwathi5@gmail.com>
2023-10-14 22:30:43 +0000

    Implemented pycodestyle for file_storage.py

--- console.py
+++ console.py
@@ -1,215 +1,160 @@
 #!/usr/bin/python3
 """
-Command interpreter for Holberton AirBnB project
+This module defines the console for the AirBnB clone project
 """
+
 import cmd
 from models.base_model import BaseModel
-from models.user import User
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
 
-CNC = {
+storage = FileStorage()
+storage.CLASSES = {
     'BaseModel': BaseModel,
     'User': User,
     'State': State,
     'City': City,
     'Amenity': Amenity,
     'Place': Place,
-    'Review': Review
+    'Review': Review,
 }
 
+
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter class"""
-    prompt = '(hbnb) '
-    ERR = [
-        '** class name missing **',
-        "** class doesn't exist **",
-        '** instance id missing **',
-        '** no instance found **',
-        '** attribute name missing **',
-        '** value missing **',
-    ]
+    """
+    The command interpreter class
+    """
+    prompt = "(hbnb) "
 
-    def default(self, line):
-        """default response for unknown commands"""
-        pass
-
-    def emptyline(self):
-        """Called when an empty line is entered in response to the prompt."""
-        pass
-
-    def __class_err(self, arg):
-        """private: checks for missing class or unknown class"""
-        error = 0
-        if len(arg) == 0:
-            print(HBNBCommand.ERR[0])
-            error = 1
-        else:
-            if arg[0] not in CNC:
-                print(HBNBCommand.ERR[1])
-                error = 1
-        return error
-
-    def __id_err(self, arg):
-        """private checks for missing ID or unknown ID"""
-        error = 0
-        if (len(arg) < 2):
-            print(HBNBCommand.ERR[2])
-            error = 1
-        return error
-
-    def do_quit(self, line):
-        """quit: quit
-        USAGE: Command to quit the program
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
         """
         return True
 
-    def do_EOF(self, line):
-        """function to handle EOF"""
-        print()
+    def do_EOF(self, arg):
+        """
+        Exit the console at end of file (EOF)
+        """
         return True
 
+    def emptyline(self):
+        """
+        Do nothing on empty input line
+        """
+        pass
+
+    def help_quit(self):
+        """
+        Print help message for the quit command
+        """
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """
+        Print help message for the EOF command
+        """
+        print("Exit the console at end of file (EOF)")
+
     def do_create(self, arg):
-        """create: create [ARG]
-        ARG = Class Name
-        SYNOPSIS: Creates a new instance of the Class from given input ARG"""
-        arg = arg.split()
-        error = self.__class_err(arg)
-        if not error:
-            for k, v in CNC.items():
-                if k == arg[0]:
-                    my_obj = v()
-                    my_obj.save()
-                    print(my_obj.id)
+        """
+        Create a new instance of BaseModel,
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in storage.CLASSES.keys():
+            print("** class doesn't exist **")
+        else:
+            new_instance = storage.CLASSES[arg]()
+            new_instance.save()
+            print(new_instance.id)
 
     def do_show(self, arg):
-        """show: show [ARG] [ARG1]
-        ARG = Class
-        ARG1 = ID #
-        SYNOPSIS: Prints object of given ID from given Class"""
-        arg = arg.split()
-        error = self.__class_err(arg)
-        if not error:
-            error += self.__id_err(arg)
-        if not error:
-            fs_o = FileStorage.all()
-            for k, v in fs_o.items():
-                if arg[1] in k and arg[0] in k:
-                    print(v)
-                    return
-            print(HBNBCommand.ERR[3])
-
-    def do_all(self, arg):
-        """all: all [ARG]
-        ARG = Class
-        SYNOPSIS: prints all objects of given class"""
-        arg = arg.split()
-        error = 0
-        if arg:
-            error = self.__class_err(arg)
-        if not error:
-            print('[', end='')
-            fs_o = FileStorage.all()
-            l = 0
-            if arg:
-                for v in fs_o.values():
-                    if type(v).__name__ == CNC[arg[0]].__name__:
-                        l += 1
-                c = 0
-                for v in fs_o.values():
-                    if type(v).__name__ == CNC[arg[0]].__name__:
-                        c += 1
-                        print(v, end=(', ' if c < l else ''))
+        """
+        Print the string representation of an instance
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in storage.CLASSES.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key in objects:
+                print(objects[key])
             else:
-                l = len(fs_o)
-                c = 0
-                for v in fs_o.values():
-                    print(v, end=(', ' if c < l else ''))
-            print(']')
+                print("** no instance found **")
 
     def do_destroy(self, arg):
-        """destroy: destroy [ARG] [ARG1]
-        ARG = Class
-        ARG1 = ID #
-        SYNOPSIS: destroys object of given ID from given Class"""
-        arg = arg.split()
-        error = self.__class_err(arg)
-        if not error:
-            error += self.__id_err(arg)
-        if not error:
-            fs_o = FileStorage.all()
-            for k in fs_o.keys():
-                if arg[1] in k and arg[0] in k:
-                    del fs_o[k]
-                    FileStorage.save(self)
-                    return
-            print(HBNBCommand.ERR[3])
-
-    def __rreplace(self, s, l):
-        for c in l:
-            s = s.replace(c, '')
-        return s
-
-    def __check_dict(self, arg):
-        """checks if the arguments input has a dictionary"""
-        if '{' and '}' in arg:
-            l = arg.split('{')[1]
-            l = l.split(', ')
-            l = list(s.split(':') for s in l)
-            d = {}
-            for subl in l:
-                k = subl[0].strip('"\' {}')
-                v = subl[1].strip('"\' {}')
-                d[k] = v
-            return d
+        """
+        Delete an instance based on class name and ID
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in storage.CLASSES.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
         else:
-            return None
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Print all string representations of instances,
+        """
+        args = arg.split()
+        objects = storage.all()
+        if not arg:
+            print([str(obj) for obj in objects.values()])
+        elif args[0] not in storage.CLASSES.keys():
+            print("** class doesn't exist **")
+        else:
+            filtered_objects = [
+                str(obj) for obj in objects.values()
+                if obj.__class__.__name__ == args[0]
+            ]
+            print(filtered_objects)
 
     def do_update(self, arg):
-        """update: update [ARG] [ARG1] [ARG2] [ARG3]
-        ARG = Class
-        ARG1 = ID #
-        ARG2 = attribute name
-        ARG3 = value of new attribute
-        SYNOPSIS: updates or adds a new attribute and value of given Class"""
-        d = self.__check_dict(arg)
-        arg = self.__rreplace(arg, [',', '"'])
-        arg = arg.split()
-        error = self.__class_err(arg)
-        if not error:
-            error += self.__id_err(arg)
-        if not error:
-            valid_id = 0
-            fs_o = FileStorage.all()
-            for k in fs_o.keys():
-                if arg[1] in k and arg[0] in k:
-                    valid_id = 1
-                    key = k
-            if not valid_id:
-                print(HBNBCommand.ERR[3])
+        """
+        Update an instance based on class name and ID
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in storage.CLASSES.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key in objects:
+                value = args[3][1:-1] if args[3][0] == '"' else args[3]
+                setattr(objects[key], args[2], value)
+                storage.save()
             else:
-                if len(arg) < 3:
-                    print(HBNBCommand.ERR[4])
-                elif len(arg) < 4:
-                    print(HBNBCommand.ERR[5])
-                else:
-                    if not d:
-                        avalue = arg[3].strip('"')
-                        if avalue.isdigit():
-                            avalue = int(avalue)
-                        fs_o[key].update({arg[2]: avalue})
-                        FileStorage.save(self)
-                    else:
-                        for k, v in d.items():
-                            if v.isdigit():
-                                v = int(v)
-                            fs_o[key].update({k: v})
-                        FileStorage.save(self)
+                print("** no instance found **")
 
-    def do_BaseModel(self, arg):
-        """class method with .function() syntax"""
-        self.__parse_exec('BaseModel', arg)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,54 +12,57 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
-	__file_path = "file.json"
-	__objects = {}
-	CLASSES = {}
+    __file_path = "file.json"
+    __objects = {}
+    CLASSES = {}
 
-	def all(self):
-		"""Returns the dictionary __objects"""
-		return self.__objects
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
 
-	def new(self, obj):
-		"""Sets in __objects the obj with key <obj class name>.id"""
-		key = "{}.{}".format(obj.__class__.__name__, obj.id)
-		self.__objects[key] = obj
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
-	def save(self):
-		"""Serializes __objects to the JSON file (path: __file_path)"""
-		serialized_objects = {}
-		for key, obj in self.__objects.items():
-			serialized_objects[key] = obj.to_dict()
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
 
-		with open(self.__file_path, "w") as json_file:
-			json.dump(serialized_objects, json_file)
+        with open(self.__file_path, "w") as json_file:
+            json.dump(serialized_objects, json_file)
 
-	def reload(self):
-		"""Deserializes the JSON file to __objects (if the file exists)"""
-		try:
-			with open(self.__file_path, 'r', encoding='utf-8') as file:
-				loaded_objects = json.load(file)
-			for key, value in loaded_objects.items():
-				class_name = value['__class__']
-				obj_id = value['id']
-				if class_name == 'BaseModel':
-					obj_instance = BaseModel(**value)
-				elif class_name == 'State':
-					obj_instance = State(**value)
-				elif class_name == 'City':
-					obj_instance = City(**value)
-				elif class_name == 'Amenity':
-					obj_instance = Amenity(**value)
-				elif class_name == 'Place':
-					obj_instance = Place(**value)
-				elif class_name == 'Review':
-					obj_instance = Review(**value)
-				else:
-					raise ValueError("Unknown class: {}".format(class_name))
-				self.__objects['{}.{}'.format(class_name, obj_id)] = obj_instance
-		except FileNotFoundError:
-			pass
+    def reload(self):
+        """Deserializes the JSON file to __objects (if the file exists)"""
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                loaded_objects = json.load(file)
+            for key, value in loaded_objects.items():
+                class_name = value['__class__']
+                obj_id = value['id']
+                if class_name == 'BaseModel':
+                    obj_instance = BaseModel(**value)
+                elif class_name == 'State':
+                    obj_instance = State(**value)
+                elif class_name == 'City':
+                    obj_instance = City(**value)
+                elif class_name == 'Amenity':
+                    obj_instance = Amenity(**value)
+                elif class_name == 'Place':
+                    obj_instance = Place(**value)
+                elif class_name == 'Review':
+                    obj_instance = Review(**value)
+                else:
+                    raise ValueError("Unknown class: {}".format(class_name))
+                key = '{}.{}'.format(class_name, obj_id)
+                self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
+
 
 storage = FileStorage()
 storage.reload()
