f206e11c05f7621826da2b97cd78f56a3904a54c
orumagideon <orumagideon535@gmail.com>
2023-10-15 09:51:56 -0700

    update

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the Review class, inheriting from BaseModel
+    Module defines the Review class, inheriting from BaseModel
 """
 
 from models.base_model import BaseModel
